a740a25c4b3b54ec90192c07bf2b2d6b92e430a5
Mend57 <cury.ro@gmail.com>
2025-11-14 19:04:04 +0100

    refactor

--- Pit.h
+++ Pit.h
@@ -5,7 +5,7 @@
 class Pit : public Floor {
     public:
         Pit(const int row, const int column) : Tile(row, column, "_"), Floor(row, column){}
-        bool onLeave(Tile* destTile, Character* who) override{return (destTile->getTexture() == "<" || destTile->getTexture() == "_") ? Floor::onLeave(destTile, who) : false;}
+        bool onLeave(Tile* destTile, Character* who) override{return (destTile->getTexture() == "<" || destTile->getTexture() == "_") ? Tile::onLeave(destTile, who) : false;}
 
 };
 
